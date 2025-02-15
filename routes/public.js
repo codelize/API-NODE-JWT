fe55e0aa872944b46f3e15b62f2cd4e9758d9114
codelize <lflizier@gmail.com>
2025-02-15 12:23:10 -0300

    feat: implement user validation and password check

--- routes/public.js
+++ routes/public.js
@@ -24,12 +24,41 @@ router.post('/cadastro', async (req, res) => {
   res.status(201).json(userDB)
   } catch(err) {
     res.status(500).json({message: "Erro no servidor, tente novamente"})
-  }
+  } 
 })
 
 
 //LOGIN
+router.post('/login', async (req, res) => {
+  
+  try{
+    const userInfo = req.body 
 
+    // Busca o usuário no banco de dados
+    const user = await prisma.user.findUnique({ 
+      where: { email: userInfo.email },
+    })
+
+    // Verifica se o usuário existe dentro do banco
+    if (!user){
+      return res.status(404).json({message: 'Usuário não enconstrado' })
+    }
+
+    // Compara a senha do banco com a que o usuário digitou
+    const isMatch = await bcrypt.compare(userInfo.password, user.password) 
+
+    if (!isMatch){
+        return res.status(400).json({ message: 'Senha inválida' })
+    }
+
+    // Gerar o token JWT
+
+
+    res.status(200).json(user)
+  } catch(err) {
+    res.status(500).json({message: "Erro no servidor, tente novamente"})
+  }
+})
 
 
 
